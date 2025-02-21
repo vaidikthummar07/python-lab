ad7a46d250f24904e0c9076893d93a168a98ab93
vaidikthummar07 <24BEC120@sot.pdpu.ac.in>
2025-02-21 12:21:43 +0530

    Add files via upload

---
+++ 4_4.py
@@ -0,0 +1,53 @@
+num=int(input("enter a number:"))
+count1=0
+count2=0
+count3=0
+check_num=0
+pnum=0
+a=str(num)
+no_digits=len(a)
+#prime number
+for i in range(2,num+1):
+    if num%i==0:
+        count1+=1
+if count1==1:
+    print(f"{num}number is prime.")
+else:
+    print(f"{num} number not is prime.")
+#perfect 
+for i in range(1,num):
+    if num%i==0:
+        count2+=i
+if count2==num:
+    print(f"{num} number is perfect.")
+else:
+    print(f"{num} number is not perfect.")
+
+#armstrong number
+num2 = num
+while(num2):
+    digit=num2%10
+    count3+=(digit**no_digits)
+    num2 = num2//10
+if count3==num:
+    print (f"{num} number is armstrong.")
+else:
+    print(f"{num} number is not armstrong.")
+
+#palinedrome
+num3=num
+while (num3):
+    pnum=(pnum*10)+(num3%10)
+    num3=num3//10
+if pnum==num:
+    print (f"{num} number is palindrome.")
+else:
+    print(f"{num} number is not palindrome.")
+
+#automorphic
+num4=num*num
+last_digit=num4%(10**no_digits)
+if last_digit==num:
+    print(f"{num} is an automorphic number.")
+else:
+    print(f"{num} is not an automorphic number.")
